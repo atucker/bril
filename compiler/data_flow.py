86eebac4a8e4dcc29ee5b8b915d6507764dda8df
Aaron Tucker <aarondtucker@gmail.com>
2022-02-14 17:58:21 -0500

    implement a reachability analysis

--- compiler/data_flow.py
+++ compiler/data_flow.py
@@ -1,5 +1,8 @@
 import json
 import sys
+import cfg
+from collections import defaultdict, OrderedDict
+from functools import reduce
 
 """
 in[entry] = init
@@ -14,10 +17,93 @@ while worklist is not empty:
         worklist += successors of b
 """
 
+NULL = "∅"
 
-def do_liveness():
+
+def get_predecessors(successors):
+    ans = {}
+    for name, _ in successors.items():
+        ans[name] = []
+    for name, ss in successors.items():
+        for successor in ss:
+            ans[successor].append(name)
+    return ans
+
+
+def get_defined(instrs):
+    defined = set()
+    for instr in instrs:
+        if 'dest' in instr:
+            defined = defined | {instr['dest']}
+    return defined
+
+
+def join_vars(left, right):
+    ans = OrderedDict()
+    for key in left.keys():
+        ans[key] = left[key]
+    for key in right.keys():
+        if key in ans:
+            ans[key] |= right[key]
+        else:
+            ans[key] = right[key]
+    return ans
+
+
+def stringify_var_version_set(var_varsion_set):
+    ans = ""
+    if len(var_varsion_set.keys()) == 0:
+        return NULL
+    for var, versions in var_varsion_set.items():
+        assert len(versions) > 0
+        for ver in versions:
+            ans += f"{var}_{ver}, "
+    return ans[:-2]
+
+
+def do_reachability():
     prog = json.load(sys.stdin)
-    
+    blocks, successors = cfg.make_cfg(prog)
+    predecessors = get_predecessors(successors)
+
+    in_reachable = {}
+    out_reachable = {}
+    # Do this manually instead of using a defaultdict so that the program
+    # crashes if I try to access something that isn't there...
+    for name in blocks.keys():
+        out_reachable[name] = {}
+
+    worklist = [*blocks.items()]
+    for name, instrs in worklist:
+        # Cache what we had before
+        prev_outpt_str = stringify_var_version_set(out_reachable[name])
+
+        # Compute our input
+        inpt = reduce(
+            join_vars, [out_reachable[p] for p in predecessors[name]], dict()
+        )
+
+        # Compute our output
+        outpt = OrderedDict(**inpt) # copy so we don't mutate the inpt
+        defined = get_defined(instrs)
+        for var in defined:
+            # If it's already there, overwrite
+            # If it's not, add it
+            outpt[var] = {name}
+
+        # Write out the values
+        in_reachable[name] = inpt
+        out_reachable[name] = outpt
+
+        # Add to the worklist if our results changed
+        if prev_outpt_str != stringify_var_version_set(outpt):
+            worklist += [(succ, blocks[succ]) for succ in successors[name]]
+
+    for name in blocks.keys():
+        print(f"{name}:")
+        print(f"  in:  {stringify_var_version_set(in_reachable[name])}")
+        print(f"  out: {stringify_var_version_set(out_reachable[name])}")
+
 
 if __name__ == "__main__":
-    do_liveness()
+    do_reachability()
