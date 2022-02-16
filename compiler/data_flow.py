56d55cc4a037b71938df21c9fd9fd76c3d7797a8
Aaron Tucker <aarondtucker@gmail.com>
2022-02-15 21:11:19 -0500

    refactor reachability to use the worklist algorithm, rather than the old implementation. Also make the variable order more predictable in string printing

--- compiler/data_flow.py
+++ compiler/data_flow.py
@@ -4,20 +4,22 @@ import cfg
 from collections import defaultdict, OrderedDict
 from functools import reduce
 
-"""
-in[entry] = init
-out[*] = init
-
-worklist = all blocks
-while worklist is not empty:
-    b = pick any block from worklist
-    in[b] = merge(out[p] for every predecessor p of b)
-    out[b] = transfer(b, in[b])
-    if out[b] changed:
-        worklist += successors of b
-"""
-
+FORWARD = "FORWARD"
+BACKWARD = "BACKWARD"
 NULL = "∅"
+
+
+class Spec:
+    def __init__(self, *, direction, init, merge, transfer):
+        """
+        init should mutate inputs
+        merge and transfer should not mutate inputs
+        """
+        assert direction in {FORWARD, BACKWARD}
+        self.direction = direction
+        self.init = init
+        self.merge = merge
+        self.transfer = transfer
 
 
 def get_predecessors(successors):
@@ -54,76 +56,107 @@ def stringify_var_version_set(var_varsion_set):
     ans = ""
     if len(var_varsion_set.keys()) == 0:
         return NULL
-    for var, versions in var_varsion_set.items():
+    for var in sorted(var_varsion_set.keys()):
+        versions = var_varsion_set[var]
         assert len(versions) > 0
         for ver in sorted(versions):
             ans += f"{var}/{ver}, "
     return ans[:-2]
 
 
-def do_reachability():
+def copy_var_version_set(var_version_set):
+    ans = {}
+    for var, versions in var_version_set.items():
+        ans[var] = set(versions)
+    return ans
+
+
+def run_worklist_algorithm(spec):
     prog = json.load(sys.stdin)
     blocks, successors = cfg.make_cfg(prog)
     predecessors = get_predecessors(successors)
 
-    assert len(sys.argv) in {1, 2}
-    mode = sys.argv[1]
-    assert mode.lower() in {'reachability'}
-
-    in_reachable = {}
-    # Do this manually instead of using a defaultdict so that the program
-    # crashes if I try to access something that isn't there...
+    block_in = {}
+    block_out = {}
     for name in blocks.keys():
-        in_reachable[name] = {}
-    out_reachable = {}
+        block_in[name] = {}
+        block_out[name] = {}
 
-    # Go through and grab the args for each function...
-    for name in blocks.keys():
-        out_reachable[name] = {}
-    for func in prog['functions']:
-        key = f"{func['name']}.entry"
-        if len(prog['functions']) == 1:
-            key = 'entry'
+    spec.init(prog, block_in, block_out)
 
-        in_reachable[key] = {}
-        if 'args' in func:
-            for arg in func['args']:
-                in_reachable[key][arg['name']] = {f"{func['name']}.arg"}
+    if spec.direction == BACKWARD:
+        block_in, block_out = block_out, block_in
+        predecessors, successors = successors, predecessors
 
     worklist = [*blocks.items()]
     for name, instrs in worklist:
         # Cache what we had before
-        prev_outpt_str = stringify_var_version_set(out_reachable[name])
+        prev_outpt_str = stringify_var_version_set(block_out[name])
 
-        # Compute our input
-        inpt = reduce(
-            join_vars, [out_reachable[p] for p in predecessors[name]], in_reachable[name]
+        block_in[name] = inpt = spec.merge(
+            block_in[name],
+            [copy_var_version_set(block_out[p]) for p in predecessors[name]]
         )
-
-        # Compute our output
-        outpt = dict(**inpt) # copy so we don't mutate the inpt
-        defined = get_defined(instrs)
-        for var in defined:
-            # If it's already there, overwrite
-            # If it's not, add it
-            outpt[var] = {name}
-
-        # Write out the values
-        in_reachable[name] = inpt
-        out_reachable[name] = outpt
+        block_out[name] = outpt = spec.transfer(
+            copy_var_version_set(inpt), name, instrs
+        )
 
         # Add to the worklist if our results changed
         if prev_outpt_str != stringify_var_version_set(outpt):
             work_names = set(w[0] for w in worklist)
             for succ in successors[name]:
                 if succ not in work_names:
-                    worklist += [(succ, blocks[succ]) for succ in successors[name]]
+                    worklist += [(succ, blocks[succ])]
+
+    if spec.direction == BACKWARD:
+        block_in, block_out = block_out, block_in
 
     for name in blocks.keys():
         print(f"{name}:")
-        print(f"  in:  {stringify_var_version_set(in_reachable[name])}")
-        print(f"  out: {stringify_var_version_set(out_reachable[name])}")
+        print(f"  in:  {stringify_var_version_set(block_in[name])}")
+        print(f"  out: {stringify_var_version_set(block_out[name])}")
+
+
+def init_add_args(prog, inpt, outpt):
+    for func in prog['functions']:
+        key = f"{func['name']}.entry"
+        if len(prog['functions']) == 1:
+            key = 'entry'
+
+        inpt[key] = {}
+        if 'args' in func:
+            for arg in func['args']:
+                inpt[key][arg['name']] = {f"{func['name']}.arg"}
+
+
+def merge(start_input, inpts):
+    return reduce(join_vars, inpts, start_input)
+
+
+def reachability_transfer(inpt, name, instrs):
+    outpt = copy_var_version_set(inpt)
+    defined = get_defined(instrs)
+    for var in defined:
+        # If it's already there, overwrite
+        # If it's not, add it
+        outpt[var] = {name}
+    return outpt
+
+
+def route_worklists():
+    assert len(sys.argv) in {1, 2}
+    mode = sys.argv[1]
+
+    if mode == 'reachability':
+        run_worklist_algorithm(Spec(
+            direction=FORWARD,
+            init=init_add_args,
+            merge=merge,
+            transfer=reachability_transfer
+        ))
+    else:
+        raise NotImplementedError()
 
 
 if __name__ == "__main__":
-    do_reachability()
+    route_worklists()
