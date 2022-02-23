01e11bad62526fb94581643bc202c10e1344b63d
Aaron Tucker <aarondtucker@gmail.com>
2022-02-23 15:52:25 -0500

    refactor cfg so that we can go function by function more easily

--- compiler/cfg.py
+++ compiler/cfg.py
@@ -33,25 +33,39 @@ def name_blocks(func_name, blocks):
     return named_blocks
 
 
+def make_func_cfg(func, func_name=''):
+    cfg = OrderedDict()
+    named_blocks = OrderedDict()
+
+    func_blocks = name_blocks(func_name, make_blocks(func['instrs']))
+    for name, block in func_blocks.items():
+        named_blocks[name] = block
+
+    for i, (name, block) in enumerate(named_blocks.items()):
+        if 'op' in block[-1] and block[-1]['op'] in {'jmp', 'br'}:
+            cfg[name] = [f"{func_name}{l}" for l in block[-1]['labels']]
+        elif 'op' in block[-1] and block[-1]['op'] == 'ret':
+            cfg[name] = []
+        else:
+            if i + 1 < len(named_blocks):
+                cfg[name] = [list(named_blocks.keys())[i + 1]]
+            else:
+                cfg[name] = []
+
+    return cfg, named_blocks
+
+
 def make_cfg(prog):
     cfg = OrderedDict()
     named_blocks = OrderedDict()
     for func in prog['functions']:
         func_name = f"{func['name']}." if len(prog['functions']) > 1 else ''
-        func_blocks = name_blocks(func_name, make_blocks(func['instrs']))
-        for name, block in func_blocks.items():
-            named_blocks[name] = block
+        func_cfg, func_blocks = make_func_cfg(func, func_name)
+        for key, value in func_cfg.items():
+            cfg[key] = value
+        for key, value in func_blocks.items():
+            named_blocks[key] = value
 
-        for i, (name, block) in enumerate(named_blocks.items()):
-            if 'op' in block[-1] and block[-1]['op'] in {'jmp', 'br'}:
-                cfg[name] = [f"{func_name}{l}" for l in block[-1]['labels']]
-            elif 'op' in block[-1] and block[-1]['op'] == 'ret':
-                cfg[name] = []
-            else:
-                if i + 1 < len(named_blocks):
-                    cfg[name] = [list(named_blocks.keys())[i+1]]
-                else:
-                    cfg[name] = []
     return named_blocks, cfg
 
 
