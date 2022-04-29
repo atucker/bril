3ffc2e3c64800670a90ff02fd279605f756c6b65
Aaron Tucker <aarondtucker@gmail.com>
2022-04-28 21:04:46 -0400

    add a readonly mode for (hopefully) faster execution, break out the solution substitution as its own thing, and add or and adn

--- compiler/synthesis.py
+++ compiler/synthesis.py
@@ -9,6 +9,8 @@ GRAMMAR = """
 ?sum: term
   | sum "+" term        -> add
   | sum "-" term        -> sub
+  | sum "|" term        -> or
+  | sum "&" term        -> and   
 
 ?term: item
   | term "*"  item      -> mul
@@ -50,43 +52,57 @@ def merge_dicts(d1, d2):
     return new
 
 
-def z3_interp(tree, lookup=None):
+def z3_interp(tree, lookup=None, readonly=False):
     op = tree.data
     if op in ('add', 'sub', 'mul', 'div', 'shl', 'shr'):
         lhs, lhvars = z3_interp(tree.children[0], lookup)
         rhs, rhvars = z3_interp(tree.children[1], lookup)
         if op == 'add':
-            return lhs + rhs, merge_dicts(lhvars, rhvars)
+            return lhs + rhs, lookup if readonly else merge_dicts(lhvars, rhvars)
         elif op == 'sub':
-            return lhs - rhs, merge_dicts(lhvars, rhvars)
+            return lhs - rhs, lookup if readonly else merge_dicts(lhvars, rhvars)
         elif op == 'mul':
-            return lhs * rhs, merge_dicts(lhvars, rhvars)
+            return lhs * rhs, lookup if readonly else merge_dicts(lhvars, rhvars)
         elif op == 'div':
-            return lhs / rhs, merge_dicts(lhvars, rhvars)
+            return lhs / rhs, lookup if readonly else merge_dicts(lhvars, rhvars)
         elif op == 'shl':
-            return lhs << rhs, merge_dicts(lhvars, rhvars)
+            return lhs << rhs, lookup if readonly else merge_dicts(lhvars, rhvars)
         elif op == 'shr':
-            return lhs >> rhs, merge_dicts(lhvars, rhvars)
+            return lhs >> rhs, lookup if readonly else merge_dicts(lhvars, rhvars)
+        elif op == 'or':
+            return lhs & rhs, lookup if readonly else merge_dicts(lhvars, rhvars)
+        elif op == 'and':
+            return lhs | rhs, lookup if readonly else merge_dicts(lhvars, rhvars)
     elif op == 'neg':
         sub, vs = z3_interp(tree.children[0], lookup)
         return -sub, vs
     elif op == 'num':
-        return int(tree.children[0]), dict(**lookup) if lookup else {}
+        if not readonly:
+            return int(tree.children[0]), dict(**lookup) if lookup else {}
+        else:
+            return int(tree.children[0]), lookup
     elif op == 'var':
         name = tree.children[0]
         var = lookup[name] if lookup and name in lookup else z3.BitVec(name, 64)
-        return var, {name: var}
+        return var, lookup if readonly else {name: var}
     elif op == 'if':
         cond, cvars = z3_interp(tree.children[0], lookup)
         true, tvars = z3_interp(tree.children[1], lookup)
         false, fvars = z3_interp(tree.children[2], lookup)
         expr = (cond != 0) * true + (cond == 0) * false
-        return expr, merge_dicts(cvars, merge_dicts(tvars, fvars))
+        return expr, lookup if readonly else merge_dicts(cvars, merge_dicts(tvars, fvars))
 
 
 def interp(tree):
     output, vars = z3_interp(tree)
     return output
+
+
+def substitute_solution(sketch_expr, sketch_vars, model):
+    variables = {}
+    for key, item in sketch_vars.items():
+        variables[key] = model.eval(item)
+    return z3_interp(sketch_expr, variables)[0]
 
 
 def synthesize(spec, sketch):
